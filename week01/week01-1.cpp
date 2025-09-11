e8eeaeca73fcaf80b38bdd895728e9370d40e115
coffra <wanghaba25@gmail.com>
2025-09-11 17:34:07 +0800

    week01

---
+++ week01/week01-1.cpp
@@ -0,0 +1,13 @@
+//week01-1
+//haystack:sadbutsad
+//needle:sad
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int H=haystack.length(),N=needle.length();
+        for(int i=0;i<=H-N;i++){
+            if(haystack.substr(i,N)==needle)return i;
+        }
+        return -1;
+    }
+};
---
+++ week01/week01-2b.py
@@ -0,0 +1,6 @@
+#week01-2b.py
+#leetcode28
+#大海撈針
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle)

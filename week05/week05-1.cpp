a7cc2de1831a58b0d5313b83451f2f423ee1c8ea
coffra <wanghaba25@gmail.com>
2025-10-09 17:11:40 +0800

    week05

---
+++ week05/week05-1.cpp
@@ -0,0 +1,12 @@
+//week05-1.cpp
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        while(ss>>word){
+
+        }
+        return word.length();
+    }
+};

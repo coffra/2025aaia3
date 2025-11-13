362e1a5e05280e7aa3b4cbd82391e170143fb169
coffra <wanghaba25@gmail.com>
2025-11-13 17:38:07 +0800

    update

---
+++ week10/week10-2.cpp
@@ -0,0 +1,16 @@
+//week10-2.cpp
+//1491. Average Salary Excluding the Minimum and Maximum Salary
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        double total=0;
+        int N=salary.size();
+        int M=salary[0],m=salary[0];
+        for (int i=0;i<N;i++){
+            total+=salary[i];
+            if(salary[i]>M) M=salary[i];
+            if(salary[i]<m) m=salary[i];
+        }
+        return (total-M-m)/(N-2);
+    }
+};
